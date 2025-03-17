c7f8c6382ecb8217d326c2a74746c7910d46410c
veekthorcodes <vamadi@onpointserv.com>
2025-03-17 13:19:16 +0000

    packaging to github

---
+++ upload-to-azure-blob.sh
@@ -0,0 +1,5 @@
+az storage blob upload --container-name forge-remote \
+  --file ~/.m2/repository/com/onpointserv/forge-remote/1.0.0/forge-remote-1.0.0.pom \
+  --name com/onpointserv/forge-remote/1.0.0/forge-remote-1.0.0.pom \
+  --account-name onpointservpackages \
+  --account-key h+xkuUczFCpbfK11/oOOSuq1IXN7GpCfkxnygA5ttG/ynNlKo+3CPV01WKVeQQ+Kf5mdda1S5HF7+ASt6vsD4A==
